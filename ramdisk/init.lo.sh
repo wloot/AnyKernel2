2a3db1c961416f3b9bd7c80e934d0e14d12c4deb
Julian Liu <wlootlxt123@gmail.com>
2019-07-25 02:15:13 +0800

    Limit max freq to 2.1ghz for big cores

--- ramdisk/init.lo.sh
+++ ramdisk/init.lo.sh
@@ -7,6 +7,8 @@ function write() {
 {
     sleep 10
 
+    write  /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2112000
+
     # Set the default IRQ affinity to the silver cluster
     write /proc/irq/default_smp_affinity f
 
