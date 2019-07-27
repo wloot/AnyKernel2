0fdfd8628e1356e4600563e2750eedad7d34c90c
Julian Liu <wlootlxt123@gmail.com>
2019-07-27 20:54:01 +0800

    Re-enable perf

--- ramdisk/init.lo.sh
+++ ramdisk/init.lo.sh
@@ -7,7 +7,7 @@ function write() {
 {
     sleep 10
 
-    write  /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2112000
+    write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2112000
 
     # Set the default IRQ affinity to the silver cluster
     write /proc/irq/default_smp_affinity f
@@ -17,4 +17,12 @@ function write() {
 
     # Disable thermal hotplug for thermal
     write /sys/module/msm_thermal/core_control/enabled 0
+
+    # Flash doesn't have back seek problem, so penalty is as low as possible
+    write /sys/block/sda/queue/iosched/back_seek_penalty 1
+
+    # UFS 2.0+ hardware queue depth is 32
+    write /sys/block/sda/queue/iosched/quantum 16
+
+    write /dev/stune/top-app/schedtune.sched_boost 10
 }&
