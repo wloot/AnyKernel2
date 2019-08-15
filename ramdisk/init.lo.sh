5c1b8821a1a6b2d3aa8990720119d311e32f0eb6
Julian Liu <wlootlxt123@gmail.com>
2019-08-16 03:31:39 +0800

    wait 1 min before limiting cpu freq

--- ramdisk/init.lo.sh
+++ ramdisk/init.lo.sh
@@ -7,8 +7,6 @@ function write() {
 {
     sleep 10
 
-    write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2112000
-
     # Set the default IRQ affinity to the silver cluster
     write /proc/irq/default_smp_affinity f
 
@@ -25,4 +23,8 @@ function write() {
     write /sys/block/sda/queue/iosched/quantum 16
 
     write /dev/stune/top-app/schedtune.sched_boost 10
+
+    # wait 1 min before limiting cpu freq
+    sleep 60
+    write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2112000
 }&
