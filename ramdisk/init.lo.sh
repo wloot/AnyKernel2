3652affa57f6349290c75aaad9d93dc77b4695bc
Julian Liu <wlootlxt123@gmail.com>
2019-08-24 04:22:08 +0800

    don't wait 10s

--- ramdisk/init.lo.sh
+++ ramdisk/init.lo.sh
@@ -5,26 +5,25 @@ function write() {
 }
 
 {
-    sleep 10
-
-    # Set the default IRQ affinity to the silver cluster
-    write /proc/irq/default_smp_affinity f
-
-    # Set interaction lock idle time
-    write /sys/devices/virtual/graphics/fb0/idle_time 100
-
-    # Disable thermal hotplug for thermal
-    write /sys/module/msm_thermal/core_control/enabled 0
-
-    # Flash doesn't have back seek problem, so penalty is as low as possible
-    write /sys/block/sda/queue/iosched/back_seek_penalty 1
-
-    # UFS 2.0+ hardware queue depth is 32
-    write /sys/block/sda/queue/iosched/quantum 16
-
-    write /dev/stune/top-app/schedtune.sched_boost 10
-
     # wait 1 min before limiting cpu freq
     sleep 60
     write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2361600
 }&
+
+# Set the default IRQ affinity to the silver cluster
+write /proc/irq/default_smp_affinity f
+
+# Set interaction lock idle time
+write /sys/devices/virtual/graphics/fb0/idle_time 100
+
+# Disable thermal hotplug for thermal
+write /sys/module/msm_thermal/core_control/enabled 0
+
+# Flash doesn't have back seek problem, so penalty is as low as possible
+write /sys/block/sda/queue/iosched/back_seek_penalty 1
+
+# UFS 2.0+ hardware queue depth is 32
+write /sys/block/sda/queue/iosched/quantum 16
+
+write /dev/stune/top-app/schedtune.sched_boost 10
+
